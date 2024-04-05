efdfa977d72380efd2829987ef4d6a311c3e2ce4
mongcheng <46275621+MongMong11@users.noreply.github.com>
2024-04-05 20:22:38 +0800

    Update 240117_iNat_data_exploration_plot_code.R

--- 240117_iNat_data_exploration_plot_code.R
+++ 240117_iNat_data_exploration_plot_code.R
@@ -11,34 +11,39 @@ showtext_auto()
 font_add("Microsoft_JhengHei", regular = "C:/Windows/Fonts/msjh.ttc")
 font_add("Microsoft_JhengHei_bold", regular = "C:/Windows/Fonts/msjhbd.ttc")
 
+#fread
 #input files
 file_name <- list.files("./result/", recursive=TRUE, full.names = TRUE, all.files = TRUE, include.dirs = TRUE, no..= FALSE)
 
-all.data<-lapply(file_name, function(x)
+iNat_dt<-lapply(file_name, function(x)
     fread(x, sep = ",", colClasses = "character", encoding= "UTF-8"))%>%
   do.call(rbind, .)
 
 #plot 1: bar plot of the data numbers by state and county
 
 #all.data re-subset & chainng
-plot1_data<-all.data[,.N, by=.(county, municipality)]
 
+#plot1_data
+        
+plot1_data<-iNat_dt %>%
+                .[,.N, by=.(county, municipality)] %>%
+                .[,.(N.Sum=sum(N)), by=county] %>%
+                .[,county := ifelse(county=="", "無資訊", county)] %>%
+                .[, county2 := factor(county, levels=c("連江縣", "金門縣", "澎湖縣", "台東縣", "花蓮縣", "宜蘭縣",
+                                                       "屏東縣", "高雄市", "台南市", "嘉義縣", "嘉義市", "雲林縣",
+                                                       "彰化縣", "南投縣", "台中市", "苗栗縣", "新竹縣", "新竹市",
+                                                       "桃園市", "新北市", "台北市", "基隆市", "無資訊"))] %>%
+                .[, N.sum := round(N.Sum/1000, digits=2)]
 
-plot1_data2<-plot1_data[,.(N.Sum=sum(N)), by=county]
-plot1_data2$county<-ifelse(plot1_data2$county=="",
-                           "無資訊",plot1_data2$county)
-plot1_data2$county2 <- factor(plot1_data2$county, levels=c("連江縣", "金門縣", "澎湖縣", "台東縣", "花蓮縣", "宜蘭縣",
-                                                           "屏東縣", "高雄市", "台南市", "嘉義縣", "嘉義市", "雲林縣",
-                                                           "彰化縣", "南投縣", "台中市", "苗栗縣", "新竹縣", "新竹市",
-                                                           "桃園市", "新北市", "台北市", "基隆市", "無資訊"))
-plot1_data2$N.Sum<-round(plot1_data2$N.Sum/1000, digits=2)
-plot1_data3 <- plot1_data
-plot1_data3$municipality<-ifelse(plot1_data3$municipality=="",
-                                 "無資訊",plot1_data3$municipality)
-plot1_data3 <- plot1_data3[which(plot1_data3$county!=""),]
+#plot1_data2
+
+plot1_data2<-iNat_dt %>%
+                .[,.N, by=.(county, municipality)] %>%
+                .[, municipality := ifelse(municipality=="", "無資訊", municipality)] %>%
+                filter(county != "")
 
 #plot1-1: state
-plot_state<-ggplot(plot1_data2, aes(x=county2, y=N.Sum)) +
+plot_state<-ggplot(plot1_data, aes(x=county2, y=N.Sum)) +
   geom_bar(stat="identity", fill="steelblue")+
   xlab("縣市")+
   ylab("資料筆數(千)")+
@@ -50,18 +55,16 @@ plot_state<-ggplot(plot1_data2, aes(x=county2, y=N.Sum)) +
   )+ coord_flip()
 
 #plot1-2: county
-datalist = list()
-for (i in 1:length(unique(plot1_data3$county))) {
-  #list
-  list<-unique(plot1_data3$county)
-  #input
-  table<-plot1_data3[grepl(list[i],county)]
-  datalist[[i]] <- table
-}
 
-for (i in 1:length(unique(plot1_data3$county))) {
-  table<-datalist[[i]]
-  #  table$N<-round(table$N/100, digits = 2)
+county_ls<-unique(plot1_data2$county)
+
+plot1_data2_ls<-lapply(1:length(county_ls), function(i){
+  table<-plot1_data2 %>% .[county==county_ls[i]] %>% setDT()
+  return(table)
+})
+
+for (i in 1:length(county_ls)) {
+  table<-plot1_data2_ls[[i]]
   state<-unique(table$county)
   p<-ggplot(table, aes(x=municipality, y=N)) +
     geom_bar(stat="identity", fill="grey")+
@@ -86,9 +89,9 @@ p.all<-grid.arrange(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10,
 p.all2<-grid.arrange(plot_state,p.all, ncol = 2)
 
 #plot 2: histogram plot of the iNat's elevation distribution
-plot2_data<-all.data[which(all.data$minimumElevationInMeters!=""),]
 
-
+plot2_data<-iNat_dt %>% filter(minimumElevationInMeters != "")
+                
 ggplot(plot2_data, aes(x=minimumElevationInMeters)) + 
   geom_histogram(aes(y=..density..), colour="black", fill="white", bins = 1000)+
   geom_density(alpha=.2, fill="#FF6666")+
@@ -97,30 +100,18 @@ ggplot(plot2_data, aes(x=minimumElevationInMeters)) +
   scale_x_continuous(breaks=seq(0,4000,500),limits=c(0, 4000))+
   theme_bw(base_size = 20)
 
-#task 3: originalVernacularName vertification list
-ver_list<-unique(all.data$originalVernacularName)
-table<-data.frame(matrix(NA,length(ver_list),2))
-colnames(table)<-c("ID", "scientific_name")
-table[,1]<-1:length(ver_list)
-table[,2]<-ver_list
-View(table)
-fwrite(table, "D:/Mong Chen/240110_iNat to OP/output/scientificname_vertification_list.csv")
+#### 3: originalVernacularName vertification list ####
 
-x<-table
-x_split<-split(x, rep(1:ceiling(nrow(x)/5000), each=5000, length.out=nrow(x)))
-dir.create("output/vertification_list")
-for (i in 1:ceiling(nrow(x)/5000)) {
-  table<-as.data.table(x_split[[i]])
-  fwrite(table, sprintf("output/vertification_list/verlist_split_%s.csv", i))
-}
+iNat_dt %>% dplyr::select(originalVernacularName) %>%
+  mutate(ID = row_number()) %>%
+  rename(scientific_name="originalVernacularName") %>%
+  fwrite(., "table/scientificname_vertification_list.csv")
 
-#sample
-random_ls<-sample(1:nrow(all.data), size = 1000)
-table<-data.frame(matrix(NA,1000,19))
-colnames(table)<-colnames(all.data)
+#### 4. random table ####
 
-for (i in 1:1000) {
-  table[i,]<-all.data[random_ls[i],]
-}
-head(table)
-fwrite(table, "D:/Mong Chen/240110_iNat to OP/output/random_table.csv")
+random_ls<-sample(1:nrow(iNat_dt), size = 1000)
+
+random_table<-lapply(1:length(random_ls), function(i){
+  return(iNat_dt[random_ls[i],])
+}) %>% do.call(rbind, .) %>%
+  write.csv(., "table/random_table.csv", fileEncoding = "UTF-8", row.names=FALSE)
